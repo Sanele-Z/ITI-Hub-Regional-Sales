a83737566a9631aec0cb05743ff91d86da29564d
Sanele.Z <zwanesmzd@gmail.com>
2020-04-30 23:58:25 +0200

    Add project files.

---
+++ Total_Sales_App/frmTotal_Sales.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Total_Sales_App
+{
+    partial class frmTotal_Sales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstRegionalSales = new System.Windows.Forms.ListBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblRegionalSales = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstRegionalSales
+            // 
+            this.lstRegionalSales.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstRegionalSales.FormattingEnabled = true;
+            this.lstRegionalSales.ItemHeight = 16;
+            this.lstRegionalSales.Location = new System.Drawing.Point(12, 80);
+            this.lstRegionalSales.Name = "lstRegionalSales";
+            this.lstRegionalSales.Size = new System.Drawing.Size(764, 164);
+            this.lstRegionalSales.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(689, 272);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(87, 43);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblRegionalSales
+            // 
+            this.lblRegionalSales.AutoSize = true;
+            this.lblRegionalSales.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegionalSales.Location = new System.Drawing.Point(12, 44);
+            this.lblRegionalSales.Name = "lblRegionalSales";
+            this.lblRegionalSales.Size = new System.Drawing.Size(202, 20);
+            this.lblRegionalSales.TabIndex = 2;
+            this.lblRegionalSales.Text = "ITI Hub Regional Sales";
+            // 
+            // frmTotal_Sales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 333);
+            this.Controls.Add(this.lblRegionalSales);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lstRegionalSales);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmTotal_Sales";
+            this.Text = "ITI Hub Regional Sales";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstRegionalSales;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblRegionalSales;
+    }
+}
+
---
+++ Total_Sales_App/frmTotal_Sales.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Total_Sales_App
+{
+    public partial class frmTotal_Sales : Form
+    {
+        public frmTotal_Sales()
+        {
+            InitializeComponent();
+
+            lstRegionalSales.Items.Add("Month       Kwazulu-Natal sales(R)      Gauteng sales(R)      Western Cape sales(R)");
+
+            int[,] salesArray = new int[6, 3]
+            {
+                { 120000, 90000, 65000 },
+                { 190000, 85000, 64000 },
+                { 175000, 80000, 71000 },
+                { 188000, 83000, 67000 },
+                { 125000, 87000, 65000 },
+                { 163000, 80000, 64000 },
+            };
+
+            int iCount = 1;
+            double dTotalSales = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    dTotalSales = dTotalSales + salesArray[i, j];
+                }
+                lstRegionalSales.Items.Add((iCount).ToString() + "              "+salesArray[i,0].ToString()+"                            "+ salesArray[i,1].ToString()+"                      "+salesArray[i,2].ToString() + "\n");
+                iCount++;
+            }
+
+            double dKznSales = 0;
+            double dGautengSales = 0;
+            double dWesternSales = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                dKznSales = dKznSales + salesArray[i, 0];
+                dGautengSales = dGautengSales + salesArray[i, 1];
+                dWesternSales = dWesternSales + salesArray[i, 2];
+                
+            }
+
+            double dKznPercentage = (dKznSales / dTotalSales)*100;
+            double dGautengPercentage = (dGautengSales / dTotalSales)*100;
+            double dWesternPercentage = (dWesternSales / dTotalSales)*100;
+
+            lstRegionalSales.Items.Add("-------------------------------------------------------------------------------------------------------");
+
+
+            lstRegionalSales.Items.Add("Total Sales= " + "  " + Math.Round(dKznPercentage) +"%                              "+ Math.Round(dKznPercentage) +"%                            "+ Math.Round(dWesternPercentage)+"%");
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
